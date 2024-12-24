7cd675943649cda34cfbfd86ad1e9fe041aa3c43
Nika Bostoganashvili <151997372+theBosto@users.noreply.github.com>
2024-12-24 19:57:19 +0400

    created useFormik hook

---
+++ src/App.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import useFormik from "./Route/hooks/useFormik";
+
+const App = () => {
+  const validate = (values) => {
+    const errors = {};
+
+    // Email validation
+    if (!values.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(values.email)) {
+      errors.email = "Invalid email address";
+    }
+
+    if (!values.password) {
+      errors.password = "Password is required";
+    } else if (
+      !/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/.test(
+        values.password
+      )
+    ) {
+      errors.password = "Invalid password";
+    }
+
+    return errors;
+  };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      console.log("Form is successfully submitted", values);
+    },
+  });
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <div>
+        <label>Email</label>
+        <input
+          type="text"
+          name="email"
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.email && formik.errors.email && (
+          <div style={{ color: "red" }}>{formik.errors.email}</div>
+        )}
+      </div>
+
+      <div>
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+        />
+        {formik.touched.password && formik.errors.password && (
+          <div style={{ color: "red" }}>{formik.errors.password}</div>
+        )}
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default App;
---
+++ src/Route/hooks/useFormik.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+const useFormik = ({ initialValues, validate, onSubmit }) => {
+  const [values, setValues] = useState(initialValues || {});
+  const [errors, setErrors] = useState({});
+  const [touched, setTouched] = useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleBlur = (e) => {
+    const { name } = e.target;
+    setTouched((prev) => ({ ...prev, [name]: true }));
+
+    if (validate) {
+      const fieldErrors = validate(values);
+      setErrors(fieldErrors);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate ? validate(values) : {};
+    setErrors(validationErrors);
+
+    if (Object.keys(validationErrors).length === 0) {
+      onSubmit(values);
+
+      resetForm();
+    }
+  };
+
+  const resetForm = () => {
+    setValues(initialValues);
+    setErrors({});
+    setTouched({});
+  };
+
+  return {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    resetForm,
+  };
+};
+
+export default useFormik;
